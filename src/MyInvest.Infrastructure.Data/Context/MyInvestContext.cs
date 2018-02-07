33200e4fad94015c2167edfbaa5d23fdcd027dea
Jonas Lima <jonas1307@live.com>
2018-02-06 23:07:07 -0200

    Importação EntityFramework + Criação DbContext

---
+++ src/MyInvest.Infrastructure.Data/Context/MyInvestContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace MyInvest.Infrastructure.Data.Context
+{
+    public class MyInvestContext : DbContext
+    {
+        #region Constructors
+
+        public MyInvestContext()
+            : base("MyInvest")
+        { }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
