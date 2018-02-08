5fe43d7fc4d0d7e5b2dd663960c26ff4d9479d39
Jonas Lima de Amorim <9701792+jonas1307@users.noreply.github.com>
2018-02-08 18:49:01 -0200

    Add some Entities

---
+++ src/MyInvest.Domain/Entities/InstituicaoFinanceira.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyInvest.Domain.Entities
+{
+    public class InstituicaoFinanceira
+    {
+        public long CodInstituicaoFinanceira { get; set; }
+
+        public string RazaoSocial { get; set; }
+
+        public string NomeFantasia { get; set; }
+
+        public string Cnpj { get; set; }
+
+        public string CodigoDeCompensacao { get; set; }
+
+        public string HomePage { get; set; }
+
+        public int CodTipoInstituicaoFinanceira { get; set; }
+
+        public virtual TipoInstituicaoFinanceira TipoInstituicaoFinanceira { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
---
+++ src/MyInvest.Domain/Entities/TipoInvestimento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyInvest.Domain.Entities
+{
+    public class TipoInvestimento
+    {
+        public int CodTipoInvestimento { get; set; }
+
+        public string Descricao { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime? DataAtualizacao { get; set; }
+    }
+}
