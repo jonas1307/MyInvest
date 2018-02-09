3ff212a313ad76c94876ea9a58cf00ddf2382073
Jonas Lima <jonas1307@live.com>
2018-02-09 00:44:49 -0200

    Change usings

--- src/MyInvest.Presentation.Mvc/Global.asax.cs
+++ src/MyInvest.Presentation.Mvc/Global.asax.cs
@@ -1,10 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace MyInvest.Presentation.Mvc
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
