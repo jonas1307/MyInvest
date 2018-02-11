91b4cb3313be7fc1004d10502da858a43a006c8c
Jonas Lima <jonas1307@live.com>
2018-02-11 15:23:25 -0200

    Interfaces and classes for Service and Repository bases

---
+++ src/MyInvest.Domain/Services/ServiceBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MyInvest.Domain.Interfaces.Repositories;
+using MyInvest.Domain.Interfaces.Services;
+
+namespace MyInvest.Domain.Services
+{
+    public class ServiceBase<TEntity> : IDisposable, IServiceBase<TEntity> where TEntity : class
+    {
+        private IRepositoryBase<TEntity> _repository;
+
+        public ServiceBase(IRepositoryBase<TEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(TEntity obj)
+        {
+            _repository.Add(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _repository.Update(obj);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _repository.Remove(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Dispose()
+        {
+            _repository = null;
+        }
+    }
+}
---
+++ src/MyInvest.Infrastructure.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using MyInvest.Domain.Interfaces.Repositories;
+using MyInvest.Infrastructure.Data.Context;
+
+namespace MyInvest.Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly MyInvestContext _context = new MyInvestContext();
+
+        public void Add(TEntity obj)
+        {
+            _context.Set<TEntity>().Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            _context.Entry(obj).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _context.Set<TEntity>().Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
