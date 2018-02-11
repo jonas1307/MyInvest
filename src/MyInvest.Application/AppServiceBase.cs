647fab7d0b21a9a5b50650aa4d3953a28a498740
Jonas Lima <jonas1307@live.com>
2018-02-11 16:16:26 -0200

    Add ServiceBase Interface and Class

---
+++ src/MyInvest.Application/AppServiceBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MyInvest.Application.Interfaces;
+using MyInvest.Domain.Interfaces.Services;
+
+namespace MyInvest.Application
+{
+    public class AppServiceBase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private IServiceBase<TEntity> _serviceBase;
+
+        public AppServiceBase(IServiceBase<TEntity> serviceBase)
+        {
+            _serviceBase = serviceBase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            _serviceBase.Add(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _serviceBase.Update(obj);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _serviceBase.Remove(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _serviceBase.GetAll();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _serviceBase.GetById(id);
+        }
+
+        public void Dispose()
+        {
+            _serviceBase = null;
+        }
+    }
+}
---
+++ src/MyInvest.Application/Interfaces/IAppServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyInvest.Application.Interfaces
+{
+    public interface IAppServiceBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(long id);
+    }
+}
