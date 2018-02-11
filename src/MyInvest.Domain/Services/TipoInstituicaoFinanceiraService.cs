9f15ea6e592603bfeab410d95a6024b29293a083
Jonas Lima <jonas1307@live.com>
2018-02-11 16:51:05 -0200

    Add TipoInstituicaoFinanceira Service implementation

---
+++ src/MyInvest.Domain/Services/TipoInstituicaoFinanceiraService.cs
@@ -0,0 +1,12 @@
+﻿using MyInvest.Domain.Entities;
+using MyInvest.Domain.Interfaces.Repositories;
+
+namespace MyInvest.Domain.Services
+{
+    public class TipoInstituicaoFinanceiraService : ServiceBase<TipoInstituicaoFinanceira>
+    {
+        public TipoInstituicaoFinanceiraService(IRepositoryBase<TipoInstituicaoFinanceira> repository) 
+            : base(repository)
+        { }
+    }
+}
