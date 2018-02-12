b804eeefb845ae4a1561a96d24281c9375a977a6
Jonas Lima <jonas1307@live.com>
2018-02-12 01:42:13 -0200

    Enabling Migrations (!)

---
+++ src/MyInvest.Infrastructure.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace MyInvest.Infrastructure.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyInvest.Infrastructure.Data.Context.MyInvestContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MyInvest.Infrastructure.Data.Context.MyInvestContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
