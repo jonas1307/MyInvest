b0290acfea7efd87109679c2fcc3c91d63ecdb00
Jonas Lima <jonas1307@live.com>
2018-02-11 22:54:01 -0200

    Add Parametrizacao CRUD

---
+++ src/MyInvest.Presentation.Mvc/Controllers/ParametrizacaoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AutoMapper;
+using MyInvest.Domain.Entities;
+using MyInvest.Domain.Interfaces.Services;
+using MyInvest.Presentation.Mvc.ViewModels;
+
+namespace MyInvest.Presentation.Mvc.Controllers
+{
+    public class ParametrizacaoController : Controller
+    {
+        private readonly ITipoInvestimentoService _tipoInvestimentoService;
+
+        public ParametrizacaoController(ITipoInvestimentoService tipoInvestimentoService)
+        {
+            _tipoInvestimentoService = tipoInvestimentoService;
+        }
+        
+        // GET: Parametrizacao/TipoInvestimento
+        public ActionResult TipoInvestimento()
+        {
+            var model = _tipoInvestimentoService.GetAll().ToList();
+
+            return View("IndexTipoInvestimento", model);
+        }
+
+        // GET: Parametrizacao/NovoTipoInvestimento
+        public ActionResult NovoTipoInvestimento()
+        {
+            return View("FormTipoInvestimento", new TipoInvestimentoViewModel());
+        }
+
+        // GET: Parametrizacao/EditarTipoInvestimento/1
+        public ActionResult EditarTipoInvestimento(long id)
+        {
+            var data = _tipoInvestimentoService.GetById(id);
+
+            if (data == null)
+                return HttpNotFound();
+
+            var model = Mapper.Map<TipoInvestimento, TipoInvestimentoViewModel>(data);
+
+            return View("FormTipoInvestimento", model);
+        }
+
+        // GET: Parametrizacao/ExcluirTipoInvestimento/1
+        public ActionResult ExcluirTipoInvestimento(long id)
+        {
+            var data = _tipoInvestimentoService.GetById(id);
+
+            if (data == null)
+                return HttpNotFound();
+
+            var model = Mapper.Map<TipoInvestimento, TipoInvestimentoViewModel>(data);
+
+            ViewBag.Title = "Exclusão de Tipo de Investimento";
+            ViewBag.Message = "Deseja realmente excluir o Tipo de Investimento abaixo?";
+
+            return View("DeleteTipoInvestimento", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarTipoInvestimento(TipoInvestimentoViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("FormTipoInvestimento", model);
+
+            var data = Mapper.Map<TipoInvestimentoViewModel, TipoInvestimento>(model);
+
+            if (model.CodTipoInvestimento == 0)
+            {
+                _tipoInvestimentoService.Add(data);
+            }
+            else
+            {
+                _tipoInvestimentoService.Update(data);
+            }
+
+            return RedirectToAction("TipoInvestimento");
+        }
+
+        [HttpPost]
+        public JsonResult ConfirmaExclusaoTipoInvestimento(long id)
+        {
+            bool sucesso;
+
+            try
+            {
+                var data = _tipoInvestimentoService.GetById(id);
+
+                _tipoInvestimentoService.Remove(data);
+
+                sucesso = true;
+            }
+            catch (Exception)
+            {
+                sucesso = false;
+            }
+
+            return Json(sucesso);
+        }
+    }
+}
---
+++ src/MyInvest.Presentation.Mvc/ViewModels/TipoInvestimentoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyInvest.Presentation.Mvc.ViewModels
+{
+    public class TipoInvestimentoViewModel
+    {
+        [Display(Name = "Código Tipo de Investimento:")]
+        public int CodTipoInvestimento { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo de Investimento:")]
+        [StringLength(150, ErrorMessage = "O nome deve conter entre {2} e {1} caracteres.", MinimumLength = 3)]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Data de Cadastro:")]
+        public DateTime? DataCadastro { get; set; }
+    }
+}
